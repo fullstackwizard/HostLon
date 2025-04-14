0919429d924a78814c714416a4782df11c42969a
Flávio Ribeiro <fullstackwizard@outlook.com>
2025-04-14 09:01:33 -0300

    feat: start front Next+React+tailShad

---
+++ frontend/hostlon-site/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import 'tw-animate-css';
+
+export default function Home() {
+  return (
+   <>
+   <Button>Hostlon!</Button>
+   </>
+  );
+}
